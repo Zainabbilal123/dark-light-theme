53df0a5ce7af4309e6f48c3899ef84a8d1bf3447
Your Name <bilalzainab123agmail.com>
2024-09-15 04:32:17 -0700

    add some code

--- java.js
+++ java.js
@@ -1,27 +1,16 @@
 
 var change = document.getElementById('change')
- 
 
 change.style.color = 'white'
 change.style.background = 'black'
-
-
-
-
-
-function color() {
+ function color() {
     var color = change.style.color === 'white' ? 'black': 'white';
     change.style.color = color
 
-
-    
-    
-    
-    var background = change.style.color === 'black' ? 'white': 'black';
+    var background = change.style.background === 'black'? 'white': 'black';
     change.style.background = background
-change.textContent = 'night'
-    
-    
-  
-   
-}
+    change.textContent = 'night'
+
+    document.body.style.backgroundColor = 'black';
+
+ }
